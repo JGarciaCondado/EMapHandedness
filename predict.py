0e78732d422e87bb5f69943eee225c34a1db726f
JGarciaCondado <jorgeschool@gmail.com>
2020-10-14 11:29:23 +0200

    Add pdb predict and generator evaluator

--- predict.py
+++ predict.py
@@ -1,4 +1,5 @@
 from tensorflow.keras.models import load_model
+from generator import Generator
 import numpy as np
 import xmippLib
 
@@ -16,24 +17,21 @@ class Predictor():
         # TODO get boxDim from model
         self.model = model
         self.boxDim = boxDim
+        self.generator = Generator('', None, None, boxDim, None)
 
     def predict_volume_class(self, volume, mask):
         """Predict volume class from volume and mask return 
         both the overall class and each prediction.
         """
         
-        Zdim, Ydim, Xdim = volume.shape
-        boxDim2 = self.boxDim//2
-        X=[]
-        for z in range(boxDim2,Zdim-boxDim2):
-            for y in range(boxDim2,Ydim-boxDim2):
-                for x in range(boxDim2,Xdim-boxDim2):
-                    if mask[z,y,x]>0:
-                        box = volume[z-boxDim2:z+boxDim2+1,y-boxDim2:y+boxDim2+1,x-boxDim2:x+boxDim2+1]
-                        X.append(box/np.linalg.norm(box))
-        X = np.array(X)#.astype('float32')
-        X = X.reshape(len(X), X.shape[1], X.shape[2], X.shape[3], 1)
+        # Empty generator boxes
+        self.generator.Boxes = []
+
+        # Populate boxes
         #TODO make mask optional
+        self.generator.createBoxes(volume, mask)
+        X = np.array(self.generator.Boxes)
+        X = X.reshape(len(X), X.shape[1], X.shape[2], X.shape[3], 1)
         box_predictions = self.predict_box_class(X)
         volume_prediction = self.consensus_voting(box_predictions)
         return volume_prediction, box_predictions
@@ -43,11 +41,24 @@ class Predictor():
         """
         return self.model.predict(boxes)
 
-    def predict_pdb_class(self, pdb, flip=False):
+    def predict_pdb_class(self, pdb, maxRes, flip=False):
         """Predict pdb class from pdb file by simulating
         electorn density and passing it through the model.
         """
-        pass
+
+        # Simulate PDB to obtain volume and mask
+        self.generator.maxRes = maxRes
+        # TODO fix use hash function inside simulate volume
+        V, Vmask = self.generator.simulate_volume(pdb, 'simpdb')
+       
+        #TODO apply flip
+
+        #TODO what if PDB simulation unssucesfull
+
+        #Obtain predictions
+        Vpred, boxpred = self.predict_volume_class(V, Vmask)
+
+        return Vpred, boxpred
 
     def evaluate_accuracy(self, targets, volumes, masks, threshold=0.5):
         """ Evaluate accuacy of model from targest, volumes and masks.
@@ -66,8 +77,25 @@ class Predictor():
         """
         return np.sum((predictions >= threshold) == targets)/targets.size
 
+    def evaluate_accuracy_generator(self, generator):
+        """ Evaluate the accuracy of the generator by boxes for an epoch 
+        """
+
+        targets, predictions = [], []
+        for i in range(len(generator)):
+            X, Y = generator[i]
+            pred = self.model.predict(X)
+            targets.append(Y)
+            predictions.append(pred)
+
+        targets, predictions = np.array(targets).flatten(), np.array(predictions).flatten() 
+        return self.evaluate_accuracy_from_predictions(targets, predictions)
+
+       
+    # TODO evaluate accuracy_generator PDB and boxes seperately
+
     def consensus_voting(self, predictions, threshold=0.5):
-        """ 
+        """ Return a class given predictions by thresholding average. 
         """
         if np.sum(predictions)/predictions.size > 0.5:
             return 1
@@ -87,3 +115,6 @@ if __name__ == '__main__':
     volume_class, boxes_class = predictor.predict_volume_class(volume, mask)
     print(volume_class, boxes_class[:10])
     print(predictor.evaluate_accuracy(np.ones(1), [volume], [mask]))
+    vpred, boxpred = predictor.predict_pdb_class('nrPDB/1EHS.pdb', 1)
+    print(vpred, boxpred[:10])
+    print(predictor.evaluate_accuracy_generator(Generator('nrPDB', 100, 100, 15, 1))) 
