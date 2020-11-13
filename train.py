bda3abfffb0092501e0c3f32f7dc20a6fb8b33e8
JGarciaCondado <jorgeschool@gmail.com>
2020-11-13 12:09:17 +0100

    Change preset values for train

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from generator import Generator
 
 fnDir = 'nrPDB'
 batch_size = 100
-batch_per_epoch = 1
+batch_per_epoch = 20
 boxDim = 15
 maxRes = 1
 
@@ -17,7 +17,7 @@ if __name__=="__main__":
     validationX, validationY = generator[0]
     checkpoint = ModelCheckpoint('detectHand_%1.1f.h5'%maxRes, monitor='loss', verbose=1, save_best_only=True)
     tensorboard = TensorBoard(log_dir="logs", histogram_freq=1, write_graph=False, write_images=False)
-    model.fit(generator,  epochs=1, verbose=1,
+    model.fit(generator,  epochs=30, verbose=1,
                         callbacks=[checkpoint, tensorboard],
                         validation_data=(validationX, validationY))
     model.save('detectHand_%1.1f.h5'%maxRes)
