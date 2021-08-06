371f7663b734568e8f2bb8b3f866ab44234b15ff
JGarciaCondado <jorgeschool@gmail.com>
2021-08-06 12:27:36 +0200

    Add processing for experimental data

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
+import mrcfile
 
 from models import HaPi
-from dataset_generator import simulate_volume
 
 # Load pipeline
 alpha_model = 'Models/5A_alpha_model.pth'
@@ -11,18 +11,24 @@ c = 5
 pipeline = HaPi(alpha_model, hand_model, box_dim, c)
 
 # Load data
-PDB = 'nrPDB/PDB/1ADG.pdb'
-SSE_type = 'alpha'
-maxRes = 5.0
-mask_threshold = 0.5
-SSE_mask_threshold = 0.5
-minresidues = 4
+PDB = '6s1n'
+EMmap = 'nrPDB/Exp_maps/maps/%s.map' % PDB
+with mrcfile.open(EMmap) as mrc:
+    Vf = mrc.data.copy()
 
-Vf, Vmask, Vmask_SSE = simulate_volume(PDB, maxRes, mask_threshold, SSE_mask_threshold, SSE_type, minresidues)
-Vf = np.flip(Vf, axis=0)
+# Threshold to create map
+Vmask = np.zeros(Vf.shape)
+thr_mask = 0.004
+Vmask[Vf>thr_mask] = 1.0
 
-# Predict hand
-thr = 0.8
+# Predict alphas
 batch_size = 2048
+alpha_probs = pipeline.model_alpha.predict_volume(Vf, Vmask, batch_size)
+with mrcfile.new('nrPDB/Exp_maps/alpha_maps/%s.map' % PDB, overwrite=True) as mrc:
+    mrc.set_data(np.zeros(alpha_probs.shape, dtype=np.float32))
+    mrc.data[:, :, :] = alpha_probs
+
+# Predict handedness
+thr = 0.5
 handness = pipeline.predict(Vf, Vmask, thr, batch_size)
-print(handness)
+print("Predicted handedness: %f" % handness)
