49e99152bd4e7170485153e1119094ffac48b146
JGarciaCondado <jorgeschool@gmail.com>
2021-08-10 12:39:32 +0200

    Add processing of experimental maps

--- pipeline.py
+++ pipeline.py
@@ -2,6 +2,7 @@ import numpy as np
 import mrcfile
 
 from models import HaPi
+from processing import process_experimental_map
 
 # Load pipeline
 alpha_model = 'Models/5A_alpha_model.pth'
@@ -11,20 +12,15 @@ c = 5
 pipeline = HaPi(alpha_model, hand_model, box_dim, c)
 
 # Load data
-PDB = '6s1n'
-EMmap = 'nrPDB/Exp_maps/maps/%s.map' % PDB
-with mrcfile.open(EMmap) as mrc:
-    Vf = mrc.data.copy()
-
-# Threshold to create map
-Vmask = np.zeros(Vf.shape)
-thr_mask = 0.004
-Vmask[Vf>thr_mask] = 1.0
+ID = 24455
+EMmap = 'nrPDB/Exp_maps/maps/emd_%d.map' % ID
+filter_res = 5.0
+Vf, Vmask = process_experimental_map(EMmap, filter_res)
 
 # Predict alphas
 batch_size = 2048
 alpha_probs = pipeline.model_alpha.predict_volume(Vf, Vmask, batch_size)
-with mrcfile.new('nrPDB/Exp_maps/alpha_maps/%s.map' % PDB, overwrite=True) as mrc:
+with mrcfile.new('nrPDB/Exp_maps/alpha_maps/%s.map' % ID, overwrite=True) as mrc:
     mrc.set_data(np.zeros(alpha_probs.shape, dtype=np.float32))
     mrc.data[:, :, :] = alpha_probs
 
