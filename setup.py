c340515b4fd31adc64d704afb8f7972e1a13b339
JGarciaCondado <jorgeschool@gmail.com>
2021-08-12 12:21:12 +0200

    Add MANIFEST file

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setuptools.setup(
     name="HaPi",
@@ -10,9 +13,8 @@ setuptools.setup(
     author_email="jorgeschool@gmail.com",
     description="A pipeline to determine the hand of cryoEM determined maps.",
     long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/JGarciaCondado/EMapHandedness",
     packages=["hapi"],
     python_requires="==3.8.5",
-    install_requires=['numpy', 'pandas', 'torch', 'mrcfile', 'scipy', 'scikit-image', 'tqdm']
+    install_requires=[requirements]
 )
